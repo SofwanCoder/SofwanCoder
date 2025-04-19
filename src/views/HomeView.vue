ad5bc9f602544b15f315748449057e32cee4c042
Sofwan Lawal <sofwancoder@gmail.com>
2025-04-19 19:23:12 +0100

    chore: add hire me on contra

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+import image from "@/assets/profile.jpg";
+import Github from "@/components/icons/Github.vue";
+import Twitter from "@/components/icons/Twitter.vue";
+import Linkedin from "@/components/icons/Linkedin.vue";
+import Resume from "@/components/icons/Resume.vue";
+import Youtube from "@/components/icons/Youtube.vue";
+import { useScriptTag } from "@vueuse/core";
+
+useScriptTag(
+  "https://contra.com/static/embed/sdk.js",
+  (el: HTMLScriptElement) => {
+    console.log(el);
+    // do something
+  },
+);
+</script>
+
 <template>
   <main>
     <div class="text-center">
@@ -57,15 +75,15 @@
           <Resume width="40" height="40" class="icon" />
         </a>
       </div>
+
+      <div class="pt-3">
+        <div
+          class="contra-hire-me-button"
+          data-analyticsUserId="a1ce4e23-230b-48e3-8fa5-be38779538a5"
+          data-theme="light"
+          data-username="sofwancoder"
+        ></div>
+      </div>
     </div>
   </main>
 </template>
-
-<script setup lang="ts">
-import image from "@/assets/profile.jpg";
-import Github from "@/components/icons/Github.vue";
-import Twitter from "@/components/icons/Twitter.vue";
-import Linkedin from "@/components/icons/Linkedin.vue";
-import Resume from "@/components/icons/Resume.vue";
-import Youtube from "@/components/icons/Youtube.vue";
-</script>
